3c06996ad4887e2dd95f2168e6da03a0e379e4a8
Kevin Dean <kdean1011@gmail.com>
2023-06-09 15:25:35 -0400

    done

--- index.js
+++ index.js
@@ -1,4 +1,60 @@
-/* Your Code Here */
+// Helper functions
+
+function createEmployeeRecord(data) {
+  return {
+    firstName: data[0],
+    familyName: data[1],
+    title: data[2],
+    payPerHour: data[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  };
+}
+
+function createEmployeeRecords(data) {
+  return data.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(dateString) {
+  const [date, hour] = dateString.split(" ");
+  this.timeInEvents.push({
+    type: "TimeIn",
+    date: date,
+    hour: parseInt(hour, 10)
+  });
+  return this;
+}
+
+function createTimeOutEvent(dateString) {
+  const [date, hour] = dateString.split(" ");
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    date: date,
+    hour: parseInt(hour, 10)
+  });
+  return this;
+}
+
+function hoursWorkedOnDate(date) {
+  const timeIn = this.timeInEvents.find(event => event.date === date).hour;
+  const timeOut = this.timeOutEvents.find(event => event.date === date).hour;
+  return (timeOut - timeIn) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+  const hoursWorked = hoursWorkedOnDate.call(this, date);
+  return hoursWorked * this.payPerHour;
+}
+
+function findEmployeeByFirstName(collection, firstName) {
+  return collection.find(employee => employee.firstName === firstName);
+}
+
+function calculatePayroll(employees) {
+  return employees.reduce((totalPayroll, employee) => {
+    return totalPayroll + allWagesFor.call(employee);
+  }, 0);
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -9,15 +65,10 @@
  for you to use if you need it!
  */
 
-const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
-
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
+ function allWagesFor() {
+  const datesWorked = this.timeInEvents.map(event => event.date);
+  return datesWorked.reduce((totalWages, date) => {
+    return totalWages + wagesEarnedOnDate.call(this, date);
+  }, 0);
 }
 
